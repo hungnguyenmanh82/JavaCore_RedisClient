cd58a26a837e77f154812898a13ec779cd0d73da
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2022-03-03 09:50:43 +0700

    update

--- src/main/java/redis/client/transaction/App1_transaction.java
+++ src/main/java/redis/client/transaction/App1_transaction.java
@@ -3,21 +3,38 @@ package redis.client.transaction;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
+import java.util.List;
+
+/**
+ * https://www.javatpoint.com/redis-transaction
+ */
 public class App1_transaction {
-	private static final Logger log = LogManager.getLogger();
+    private static final Logger log = LogManager.getLogger();
 
-	public static void main(String[] args) {
 
-		String address = "localhost";
-		int port = 6379; //default Port = 6379
+    public static void main(String[] args) {
 
-		Jedis jedis = new Jedis(address,port); 
+        String address = "localhost";
+        int port = 6379; //default Port = 6379
 
-		log.debug("Connection to server sucessfully"); 
-		//check whether server is running or not 
-		log.debug("Server is running: "+jedis.ping()); 
+        Jedis jedis = new Jedis(address, port);
 
-	}
+        log.debug("Connection to server sucessfully");
+        // STEP 1: start transaction
+        Transaction transaction = jedis.multi();
+
+        transaction.set("keyA", "valueA");
+        transaction.set("keyB", "valueB");
+
+        /**
+         * STEP 2: commit transaction
+         * results sẽ chứa kết quả thành công hay fail
+         */
+        List<Object> results = transaction.exec();
+
+        log.debug("results = {}", results);
+    }
 
 }
